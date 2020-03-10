54cacf1c83937cf8d84e4bee1a6be0173c78e02b
myname21-meet <myname21@meet.mit.edu>
2020-03-10 20:56:10 +0200

    structure

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template, request
 
-from database import *
+#from database import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    
+    return render_template("index.html")
+
+@app.route('/')
+def paintings():
+    
+    return render_template("paintings.html")
+
+@app.route('/')
+def about():
+    
+    return render_template("about.html")
+
+@app.route('/')
+def contact():
     
     return render_template("index.html")
 
